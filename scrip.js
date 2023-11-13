c519d276a46c4120988703dfff441089db9e7abe
LGHACK12 <ramosvasquezluismiguel@gmail.com>
2023-11-13 11:35:37 -0600

    cambios lenguaje

--- scrip.js
+++ scrip.js
@@ -1,20 +1,20 @@
-var operation1 = "";
-var operation2 = "";
-var operate;
+"use strict";
+let operation1 = "";
+let operation2 = "";
+let operate;
 function insert(event) {
-    var x = event.target.value;
-    // const screem: string = document.getElementById("resultado")?.innerText || "";
+    const x = event.target.value;
     if (parseFloat(x) >= 0 && !operate) {
         operation1 = operation1 + x;
-        document.getElementById("resultado").innerText = operation1;
+        document.getElementById("result").innerText = operation1;
     }
     if (operate && parseFloat(x) >= 0) {
         operation2 = operation2 + x;
-        document.getElementById("resultado").innerText = operation2;
+        document.getElementById("result").innerText = operation2;
     }
     if (!parseFloat(x) && !operation2 && x !== "0") {
         operate = x;
-        document.getElementById("resultado").innerText = operation1 + operate;
+        document.getElementById("result").innerText = operation1 + operate;
     }
     console.log("number 1", operation1);
     console.log("number 2", operation2);
@@ -22,35 +22,43 @@ function insert(event) {
 }
 function show() {
     if (operation1 && operate && operation2) {
-        document.getElementById("resultado").innerText = calc().toString();
+        document.getElementById("result").innerText = calc().toString();
     }
+    // multiplication by 0 function
     if (calc() && operate === "/" && operation2 === "0") {
-        alert("No es posible ejecutar esta acción");
+        alert("This action cannot be executed");
     }
+    //
     if (calc()) {
         operation1 = calc().toString();
         operation2 = "";
         operate = "";
     }
 }
+// value taker
 function clean() {
     operation1 = "";
     operation2 = "";
     operate = "";
-    document.getElementById("resultado").innerText = "";
+    document.getElementById("result").innerText = "";
 }
+// addition function
 function add() {
     return parseFloat(operation1) + parseFloat(operation2);
 }
+// subtractions function
 function subtract() {
     return parseFloat(operation1) - parseFloat(operation2);
 }
+// multiplication function
 function multiply() {
     return parseFloat(operation1) * parseFloat(operation2);
 }
+// divisions function
 function split() {
     return parseFloat(operation1) / parseFloat(operation2);
 }
+// function to execute
 function calc() {
     switch (operate) {
         case "+":
--- scrip.ts
+++ scrip.ts
@@ -3,78 +3,78 @@ let operation2: string = "";
 let operate: string;
 
 function insert(event: Event): void {
-    const x: string = (event.target as HTMLButtonElement).value;
-    if (parseFloat(x) >= 0 && !operate) {
-        operation1 = operation1 + x;
-        document.getElementById("resultado")!.innerText = operation1;
-    }
+  const x: string = (event.target as HTMLButtonElement).value;
+  if (parseFloat(x) >= 0 && !operate) {
+    operation1 = operation1 + x;
+    document.getElementById("result")!.innerText = operation1;
+  }
 
-    if (operate && parseFloat(x) >= 0) {
-        operation2 = operation2 + x;
-        document.getElementById("resultado")!.innerText = operation2;
-    }
+  if (operate && parseFloat(x) >= 0) {
+    operation2 = operation2 + x;
+    document.getElementById("result")!.innerText = operation2;
+  }
 
-    if (!parseFloat(x) && !operation2 && x !== "0") {
-        operate = x;
-        document.getElementById("resultado")!.innerText = operation1 + operate;
-    }
+  if (!parseFloat(x) && !operation2 && x !== "0") {
+    operate = x;
+    document.getElementById("result")!.innerText = operation1 + operate;
+  }
 
-    console.log("number 1", operation1);
-    console.log("number 2", operation2);
-    console.log("sign", operate);
+  console.log("number 1", operation1);
+  console.log("number 2", operation2);
+  console.log("sign", operate);
 }
 
 function show(): void {
-    if (operation1 && operate && operation2) {
-        document.getElementById("resultado")!.innerText = calc().toString();
-    }
-    // funcion de multiplicacion por 0
-    if (calc() && operate === "/" && operation2 === "0") {
-        alert("No es posible ejecutar esta acción");
-    }
-    // 
-    if (calc()) {
-        operation1 = calc().toString();
-        operation2 = "";
-        operate = "";
-    }
-}
-// tomador de valores
-function clean(): void {
-    operation1 = "";
+  if (operation1 && operate && operation2) {
+    document.getElementById("result")!.innerText = calc().toString();
+  }
+  // multiplication by 0 function
+  if (calc() && operate === "/" && operation2 === "0") {
+    alert("This action cannot be executed");
+  }
+  //
+  if (calc()) {
+    operation1 = calc().toString();
     operation2 = "";
     operate = "";
-    document.getElementById("resultado")!.innerText = "";
+  }
 }
-// funcion de sumar
+// value taker
+function clean(): void {
+  operation1 = "";
+  operation2 = "";
+  operate = "";
+  document.getElementById("result")!.innerText = "";
+}
+// addition function
 function add(): number {
-    return parseFloat(operation1) + parseFloat(operation2);
+  return parseFloat(operation1) + parseFloat(operation2);
 }
-// funcion de restar
+// subtractions function
 function subtract(): number {
-    return parseFloat(operation1) - parseFloat(operation2);
+  return parseFloat(operation1) - parseFloat(operation2);
 }
-// funcion de multiplicar
+// multiplication function
 function multiply(): number {
-    return parseFloat(operation1) * parseFloat(operation2);
+  return parseFloat(operation1) * parseFloat(operation2);
 }
-// funcion de divicion
+// divisions function
 function split(): number {
-    return parseFloat(operation1) / parseFloat(operation2);
+  return parseFloat(operation1) / parseFloat(operation2);
 }
 
-// evaluador de funcion a ejecutar
+// function to execute
 function calc(): number | undefined {
-    switch (operate) {
-        case "+":
-            return add();
-        case "-":
-            return subtract();
-        case "*":
-            return multiply();
-        case "/":
-            return split();
-        default:
-            return undefined;
-    }
+  switch (operate) {
+    case "+":
+      return add();
+    case "-":
+      return subtract();
+    case "*":
+      return multiply();
+    case "/":
+      return split();
+    default:
+      return undefined;
+  }
 }
